215ee774a0f11c2621435b857e82f89fcaff4fa0
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-08 01:06:16 +0630

    implement: Page Routing with React Router DOM [Login,Register,Home]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Index from "./pages/Index";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 const App = () => {
-  return (
-    <>
-      <h1 className="text-red-500">App</h1>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Index />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
