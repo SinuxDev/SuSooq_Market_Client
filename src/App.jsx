24efe1113f8319d42960404ece07cdc58a4b3b1a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-09 21:11:18 +0630

    update: route method changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/Index";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Main from "./layouts/Main";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Index />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
+      element: <Main />,
+      children: [
+        {
+          index: true,
+          element: <Index />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
