b91fedb3aa04aa49fd0eb89a6460a4372e9f736e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-12 01:41:37 +0630

    feat: Configure Redux store with Persistor for Combined Reducers

---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: {
+    reducer: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export default store;
