94b1a593b1b4339ccbbfafe7c6d305da4ee64275
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-12 01:38:30 +0630

    use: useSelector [userId]

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import { UserIcon } from "@heroicons/react/24/solid";
 
 const Nav = () => {
+  const { userId } = useSelector((state) => state.reducer.user);
   return (
     <nav className="bg-blue-500 flex items-center justify-between text-white p-4">
       <Link className="font-bold text-2xl" to={"/"}>
         SuSooq.IO
       </Link>
-      {localStorage.getItem("token") ? (
+      {userId ? (
         <Link to={"/profile"} className=" px-2 py-1 flex items-end gap-1">
           {" "}
           <UserIcon width={25} /> Profile
