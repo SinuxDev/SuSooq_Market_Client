c53213badeedd7f875d8dbbf8be7840b2b22fb29
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-14 01:12:02 +0630

    add: getProducts

--- src/pages/profile/Index.jsx
+++ src/pages/profile/Index.jsx
@@ -1,22 +1,48 @@
-import { Tabs } from "antd";
+import { Tabs, message } from "antd";
 import Products from "./Products";
 import AddProduct from "./AddProduct";
 import General from "./General";
-
-import { useState } from "react";
+import { getAllProducts } from "../../api/product";
+import { useState, useEffect } from "react";
 
 const Index = () => {
   const [activeTabKey, setActiveTabKey] = useState("1");
+
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await getAllProducts();
+
+      if (response.isSuccess) {
+        setProducts(response.products);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (err) {
+      message.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   const items = [
     {
       key: "1",
       label: "Products",
-      children: <Products />,
+      children: <Products products={products} />,
     },
     {
       key: "2",
       label: "Add Product",
-      children: <AddProduct setActiveTabKey={setActiveTabKey} />,
+      children: (
+        <AddProduct
+          setActiveTabKey={setActiveTabKey}
+          getProducts={getProducts}
+        />
+      ),
     },
     {
       key: "3",
