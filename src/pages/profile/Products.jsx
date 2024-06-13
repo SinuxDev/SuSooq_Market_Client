ac4fe35b6e9206b7b12e396e9c2c55916ed6a1ff
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-14 03:44:18 +0630

    update: design

--- src/pages/profile/Products.jsx
+++ src/pages/profile/Products.jsx
@@ -20,7 +20,7 @@ const Products = ({
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center ">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 text-left">
                 Product name
               </th>
               <th scope="col" className="px-6 py-3">
@@ -47,7 +47,7 @@ const Products = ({
                   >
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap text-left "
                     >
                       {product.name}
                     </th>
