8a79370967814f79bdb94074e8d52c52c26882ce
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-14 01:11:14 +0630

    set: display products

--- src/pages/profile/Products.jsx
+++ src/pages/profile/Products.jsx
@@ -1,9 +1,85 @@
-const Products = () => {
+import PropTypes from "prop-types";
+
+const Products = ({ products }) => {
   return (
-    <>
-      <h1>Products</h1>
-    </>
+    <section>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 text-center ">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sell Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            {products.length > 0 ? (
+              <>
+                {products.map((product) => (
+                  <tr
+                    className="odd:bg-white  even:bg-gray-50  border-b "
+                    key={product._id}
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                    >
+                      {product.name}
+                    </th>
+                    <td className="px-6 py-4"> {product.category} </td>
+                    <td className="px-6 py-4">
+                      {" "}
+                      {product.status === "pending" ? (
+                        <span className="bg-yellow-400 text-xs p-1 rounded-md text-white">
+                          {product.status}
+                        </span>
+                      ) : (
+                        <span className="bg-green-600 text-xs p-1 rounded-md text-white">
+                          {" "}
+                          {product.status}{" "}
+                        </span>
+                      )}{" "}
+                    </td>
+                    <td className="px-6 py-4"> {product.createdAt} </td>
+                    <td className="px-6 py-4">
+                      <button
+                        type="button"
+                        className="font-medium text-blue-600  hover:underline"
+                      >
+                        Edit
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            ) : (
+              <tr>
+                <td colSpan="5" className="text-center py-4">
+                  No products found
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
   );
+};
+
+Products.propTypes = {
+  products: PropTypes.any,
 };
 
 export default Products;
