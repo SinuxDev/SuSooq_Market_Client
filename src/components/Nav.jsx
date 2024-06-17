26ce1aee9991b3b8cea8753674df910e95cc2bbc
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 01:59:16 +0630

    set: Condition for [user & admin]

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,10 +11,20 @@ const Nav = () => {
         SuSooq.IO
       </Link>
       {user ? (
-        <Link to={"/profile"} className=" px-2 py-1 flex items-end gap-1">
-          {" "}
-          <UserIcon width={25} /> Profile
-        </Link>
+        <>
+          {user.role === "user" && (
+            <Link to={"/profile"} className=" px-2 py-1 flex items-end gap-1">
+              {" "}
+              <UserIcon width={25} /> Profile
+            </Link>
+          )}
+          {user.role === "admin" && (
+            <Link to={"/admin"} className=" px-2 py-1 flex items-end gap-1">
+              {" "}
+              <UserIcon width={25} /> Admin Panel
+            </Link>
+          )}
+        </>
       ) : (
         <div className="text-white flex items-center gap-3 text-base font-medium">
           <Link to={"/login"}>Login</Link>
