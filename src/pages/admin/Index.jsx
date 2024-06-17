5efe53ebae082e3ecefc15f24eb4bcf6d8eff722
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 01:59:58 +0630

    create: Index For Admin [Tabs]

---
+++ src/pages/admin/Index.jsx
@@ -0,0 +1,82 @@
+import { Tabs, message } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import Products from "./Products";
+import User from "./User";
+import { getAllProducts } from "../../api/admin";
+import General from "./General";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Index = () => {
+  const { user } = useSelector((state) => state.reducer.user);
+  const [activeTabKey, setActiveTabKey] = useState("1");
+  const [products, setProducts] = useState([]);
+
+  const navigate = useNavigate();
+
+  const onChangeHandler = (key) => {
+    setActiveTabKey(key);
+  };
+
+  const getProducts = async () => {
+    try {
+      const response = await getAllProducts();
+      if (response.isSuccess) {
+        setProducts(response.products);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (err) {
+      message.error(err.message);
+    }
+  };
+
+  const checkIsAdmin = useCallback(() => {
+    if (user.role !== "admin") {
+      message.error("You are not authorized to access this page");
+      navigate("/");
+      return;
+    }
+  }, [user.role, navigate]);
+
+  useEffect(() => {
+    getProducts();
+  }, [activeTabKey, checkIsAdmin]);
+
+  const items = [
+    {
+      key: "1",
+      label: "Manage Products",
+      children: <Products products={products} />,
+    },
+    {
+      key: "2",
+      label: "Users Management",
+      children: <User />,
+    },
+    {
+      key: "3",
+      label: "Notifications",
+    },
+    {
+      key: "4",
+      label: "General",
+      children: <General />,
+    },
+  ];
+
+  return (
+    <section>
+      <Tabs
+        activeKey={activeTabKey}
+        onChange={(key) => onChangeHandler(key)}
+        tabPosition="left"
+        size="large"
+        items={items}
+        animated={false}
+      />
+    </section>
+  );
+};
+
+export default Index;
