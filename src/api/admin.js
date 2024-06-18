01f51076b834660baec6d741e78a563d74b53f64
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-18 22:37:19 +0630

    API Call For Product [Approve & Reject]

--- src/api/admin.js
+++ src/api/admin.js
@@ -11,3 +11,33 @@ export const getAllProducts = async () => {
     return error.message;
   }
 };
+
+// Approve Product API
+export const approveProduct = async (productId) => {
+  try {
+    const response = await axiosInstance.post(
+      `/admin/products/approve/${productId}`,
+      {
+        validateStatus: () => true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+// Reject Product API
+export const rejectProduct = async (productId) => {
+  try {
+    const response = await axiosInstance.post(
+      `/admin/products/reject/${productId}`,
+      {
+        validateStatus: () => true,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
