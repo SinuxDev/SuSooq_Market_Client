55165156bdf6866dabdb14b9bfbd3777e07cd6c6
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-23 01:29:18 +0630

    add: Dynamic Product [Categories & Counts]

--- src/components/Dashboard/BarLists.jsx
+++ src/components/Dashboard/BarLists.jsx
@@ -1,21 +1,51 @@
-import { Card, BarList, Title } from "@tremor/react";
+import { Card, BarList, Title, Flex, Bold, Text } from "@tremor/react";
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
-const data = [
-  { name: "Shin", value: 100 },
-  { name: "Kyu", value: 200 },
-  { name: "Lee", value: 300 },
-];
+// BarLists => Products By Categories
 
-const BarLists = () => {
+const BarLists = ({ products }) => {
+  const [productCateogries, setProductCategories] = useState([]);
+
+  useEffect(() => {
+    const categories = {};
+    products.forEach((product) => {
+      if (!categories[product.category]) {
+        categories[product.category] = 0;
+      }
+
+      categories[product.category] += 1;
+    });
+
+    const data = Object.entries(categories).map(([category, count]) => ({
+      name: category,
+      value: count,
+    }));
+
+    setProductCategories(data);
+  }, [setProductCategories, products]);
+
   return (
     <Card className="mx-auto mt-7 w-full">
       <Title className="text-tremor-metric text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold mb-7">
         {" "}
         Product Count By Categories{" "}
       </Title>
-      <BarList data={data} className="mt-2" />
+      <Flex className="mt-4">
+        <Text>
+          <Bold> Product Categories </Bold>
+        </Text>
+        <Text>
+          <Bold> Counts </Bold>
+        </Text>
+      </Flex>
+      <BarList data={productCateogries} className="mt-2" />
     </Card>
   );
+};
+
+BarLists.propTypes = {
+  products: PropTypes.any,
 };
 
 export default BarLists;
