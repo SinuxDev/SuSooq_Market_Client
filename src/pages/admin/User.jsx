8521d4b279e023f3395c2301b0eb600e60d53303
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-22 20:33:45 +0630

    include: getUsers dependency

--- src/pages/admin/User.jsx
+++ src/pages/admin/User.jsx
@@ -7,7 +7,7 @@ import PropTypes from "prop-types";
 const User = ({ users, getUsers }) => {
   useEffect(() => {
     getUsers();
-  }, []);
+  }, [getUsers]);
 
   const banHandler = async (userId) => {
     try {
