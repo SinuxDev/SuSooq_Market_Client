090990f88f587846d2a38da907866891f4a3a26e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-24 23:47:23 +0630

    add: Icons

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -1,4 +1,9 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  CloudArrowUpIcon,
+  GlobeAltIcon,
+  LockClosedIcon,
+} from "@heroicons/react/24/outline";
 
 const Hero = () => {
   return (
@@ -20,7 +25,49 @@ const Hero = () => {
             className="text-blue-600 absolute top-2 right-2 cursor-pointer"
           />
         </div>
+        <div className="flex items-center justify-between max-w-6xl mx-auto mt-12 mb-20">
+          <div className="flex items-center flex-col w-1/3">
+            <div className="bg-blue-500 p-1 w-1/6 mx-auto rounded-xl mb-2">
+              <CloudArrowUpIcon
+                width={40}
+                height={40}
+                className="text-gray-300 mx-auto "
+              />
+            </div>
+            <p className=" text-black font-extrabold text-2xl">Customizable</p>
+            <p className="text-gray-500 font-semibold italic">
+              Free website designs to launch your store quickly and easily.
+            </p>
+          </div>
+          <div className="flex items-center flex-col w-1/3">
+            <div className="bg-blue-500 p-1 w-1/6 mx-auto rounded-xl mb-2">
+              <LockClosedIcon
+                width={40}
+                height={40}
+                className="text-gray-300 mx-auto "
+              />
+            </div>
+            <p className=" text-black font-extrabold text-2xl">Customizable</p>
+            <p className="text-gray-500 font-semibold italic">
+              Free website designs to launch your store quickly and easily.
+            </p>
+          </div>
+          <div className="flex items-center flex-col w-1/3">
+            <div className="bg-blue-500 p-1 w-1/6 mx-auto rounded-xl mb-2">
+              <GlobeAltIcon
+                width={40}
+                height={40}
+                className="text-gray-300 mx-auto "
+              />
+            </div>
+            <p className=" text-black font-extrabold text-2xl">Customizable</p>
+            <p className="text-gray-500 font-semibold italic">
+              Free website designs to launch your store quickly and easily.
+            </p>
+          </div>
+        </div>
       </div>
+      <hr className="border border-solid mb-12" />
     </>
   );
 };
