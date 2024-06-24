570bef2c6270424c1d0a518076190b4459156ebe
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-25 01:49:17 +0630

    update: Dynamic Categories

--- src/components/HomePage/Filter.jsx
+++ src/components/HomePage/Filter.jsx
@@ -1,61 +1,26 @@
-const Filter = () => {
-  const CatagoriesOptions = [
-    {
-      value: "electronics",
-      label: "Electronics",
-    },
-    {
-      value: "fashion",
-      label: "Fashion",
-    },
-    {
-      value: "home_garden",
-      label: "Home & Garden",
-    },
-    {
-      value: "health_beauty",
-      label: "Health & Beauty",
-    },
-    {
-      value: "sports_outdoors",
-      label: "Sports & Outdoors",
-    },
-    {
-      value: "toys_hobbies",
-      label: "Toys & Hobbies",
-    },
-    {
-      value: "automotive",
-      label: "Automotive",
-    },
-    {
-      value: "books_media",
-      label: "Books & Media",
-    },
-    {
-      value: "collectibles_art",
-      label: "Collectibles & Art",
-    },
-    {
-      value: "food_beverages",
-      label: "Food & Beverages",
-    },
-  ];
+import PropTypes from "prop-types";
+
+const Filter = ({ products }) => {
   return (
     <>
-      <div className="flex items-center gap-2 my-8 max-w-3xl whitespace-nowrap mx-auto flex-wrap justify-center">
-        {CatagoriesOptions.map((option) => (
+      <h1 className="font-extrabold text-center text-5xl">Categories</h1>
+      <div className="flex items-center gap-3 my-8 max-w-3xl whitespace-nowrap mx-auto flex-wrap justify-center">
+        {products.map((product) => (
           <p
-            key={option.value}
-            className="text-xs font-semibold bg-blue-600 text-white px-2 py-2 rounded-xl"
+            key={product._id}
+            className="text-base font-semibold bg-blue-600 text-white px-2 py-2 rounded-xl"
           >
             {" "}
-            {option.label}{" "}
+            {product.category.toUpperCase().replace("_", " ")}{" "}
           </p>
         ))}
       </div>
     </>
   );
+};
+
+Filter.propTypes = {
+  products: PropTypes.array.isRequired,
 };
 
 export default Filter;
