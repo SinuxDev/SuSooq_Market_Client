e9502d3b765818515bf934d54ade9b5d7f28768e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-24 20:44:48 +0630

    update: Refactor API Calls : Resuable Fun [FetchAuth]

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,40 +1,27 @@
 import { axiosInstance } from "./axiosInstance";
 
-// register new account
-export const registerUser = async (payload) => {
+// Combined API Calls [Auth]
+const FetchAuth = async (endPoint, payload = {}, method = "get") => {
   try {
-    const response = await axiosInstance.post("/register", payload, {
+    const response = await axiosInstance({
+      method,
+      url: endPoint,
+      data: payload,
       validateStatus: () => true,
     });
-
     return response.data;
-  } catch (error) {
-    return error.message;
+  } catch (err) {
+    return err.message;
   }
 };
+
+// register new account
+export const registerUser = async (payload) =>
+  FetchAuth("/register", payload, "post");
 
 // login to account
-export const loginUser = async (payload) => {
-  try {
-    const response = await axiosInstance.post("/login", payload, {
-      validateStatus: () => true,
-    });
-
-    return response.data;
-  } catch (error) {
-    return error.message;
-  }
-};
+export const loginUser = async (payload) =>
+  FetchAuth("/login", payload, "post");
 
 // check if user token is valid
-export const checkUserToken = async () => {
-  try {
-    const response = await axiosInstance.get("/checkUserToken", {
-      validateStatus: () => true,
-    });
-
-    return response.data;
-  } catch (error) {
-    return error.message;
-  }
-};
+export const checkUserToken = async () => FetchAuth("/checkUserToken");
