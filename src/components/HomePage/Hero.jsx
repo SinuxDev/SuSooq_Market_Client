6e93c8cd7cb0833f4d713b537add552ecd99cc3a
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-24 20:07:39 +0630

    update: Margin

--- src/components/HomePage/Hero.jsx
+++ src/components/HomePage/Hero.jsx
@@ -3,7 +3,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 const Hero = () => {
   return (
     <>
-      <div className="w-full text-center my-2">
+      <div className="w-full text-center my-6">
         <h1 className="text-3xl font-bold text-blue-600 mb-4">
           <q>Rich With SuSooq</q>
         </h1>
