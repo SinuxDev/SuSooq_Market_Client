2cfd4ad5913505c4df164e2531dff9af90631e75
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-24 21:18:22 +0630

    props: Cards [Products]

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -1,11 +1,37 @@
 import Hero from "../../components/HomePage/Hero";
 import Filter from "../../components/HomePage/Filter";
+import Cards from "../../components/HomePage/Cards";
+import { useCallback, useEffect, useState } from "react";
+import { getPublicProducts } from "../../api/product";
+import { message } from "antd";
 
 const Index = () => {
+  const [products, setProducts] = useState([]);
+
+  const getAllPublicProducts = useCallback(async () => {
+    try {
+      const response = await getPublicProducts();
+      if (response.isSuccess) {
+        setProducts(response.products);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (err) {
+      message.error(err.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllPublicProducts();
+  }, [getAllPublicProducts]);
+
+  console.log(products);
+
   return (
     <>
       <Hero />
       <Filter />
+      <Cards products={products} />
     </>
   );
 };
