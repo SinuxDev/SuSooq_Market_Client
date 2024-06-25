9cf3abc3b5341212dd35d530faa564bfc5ae5cf3
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-26 00:47:27 +0630

    update: Remove Duplicate Categories [Only Unique]

--- src/components/HomePage/Filter.jsx
+++ src/components/HomePage/Filter.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
 
 const Filter = ({ products }) => {
+  const uniqueCategories = [
+    ...new Set(products.map((product) => product.category)),
+  ];
+
   return (
     <>
       <h1 className="font-extrabold text-center text-5xl">Categories</h1>
       <div className="flex items-center gap-3 my-8 max-w-3xl whitespace-nowrap mx-auto flex-wrap justify-center">
-        {products.map((product) => (
+        {uniqueCategories.map((category) => (
           <p
-            key={product._id}
+            key={category}
             className="text-base font-semibold bg-blue-600 text-white px-2 py-2 rounded-xl"
           >
             {" "}
-            {product.category.toUpperCase().replace("_", " ")}{" "}
+            {category.toUpperCase().replace("_", " ")}{" "}
           </p>
         ))}
       </div>
