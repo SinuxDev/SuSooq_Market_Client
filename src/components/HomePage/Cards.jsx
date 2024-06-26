d75a9f56899035e73ecbbebb01034db7542ca30e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-27 02:38:50 +0630

    Update: Reuse Cards For Saved Products

--- src/components/HomePage/Cards.jsx
+++ src/components/HomePage/Cards.jsx
@@ -1,44 +1,89 @@
 import PropTypes from "prop-types";
 import SImage from "../../images/cardImg.png";
-import { BookmarkIcon } from "@heroicons/react/24/outline";
-import { Card } from "antd";
+import { BookmarkIcon, BookmarkSlashIcon } from "@heroicons/react/24/outline";
+import { Card, message } from "antd";
 import { Link } from "react-router-dom";
+import { saveProduct, unSaveProduct } from "../../api/product";
 
-const Cards = ({ product }) => {
+const Cards = ({ product, saved, getSaveProductLists }) => {
+  const handleProductSaveStatus = async (id, action) => {
+    try {
+      const response =
+        action === "save" ? await saveProduct(id) : await unSaveProduct(id);
+
+      if (response.isSuccess) {
+        if (action === "unsave") {
+          getSaveProductLists();
+        }
+        message.success(response.message);
+      } else {
+        throw new Error(response.message);
+      }
+    } catch (err) {
+      message.error(err.message);
+    }
+  };
+
+  const savedProductHandler = (id) => handleProductSaveStatus(id, "save");
+
+  const UnSavedProductHandler = (id) => handleProductSaveStatus(id, "unsave");
+
   return (
     <>
-      <div className="basis-1/2 px-4 mb-6">
-        <Link to={`/products/${product._id}`}>
-          <Card
-            hoverable
-            className="rounded-lg overflow-hidden shadow-lg"
-            cover={
+      <div className={`${saved ? "basis-1/3" : "basis-1/2"} px-4 mb-6`}>
+        <Card
+          hoverable
+          className="rounded-lg overflow-hidden shadow-lg"
+          cover={
+            <Link to={`/products/${product._id}`}>
               <img
-                src={product.images[0] ? product.images[0] : SImage}
+                src={
+                  product.images && product.images[0]
+                    ? product.images[0]
+                    : SImage
+                }
                 alt={product.name}
                 className="w-full h-56 object-cover"
               />
-            }
-          >
+            </Link>
+          }
+        >
+          <Link to={`/products/${product._id}`}>
             <p className="text-white text-sm bg-blue-600 rounded-lg p-2 w-fit my-2">
               {" "}
               {product.category.toUpperCase().replace("_", " ")}{" "}
             </p>
-            <div className="flex items-center justify-between">
-              <p className="text-xl font-bold">{product.name}</p>
-              <p className="text-lg font-semibold">
-                {" "}
-                Price : $ {product.price}{" "}
-              </p>
+          </Link>
+          <div className="flex items-center justify-between">
+            <p className="text-xl font-bold">{product.name}</p>
+            <p className="text-lg font-semibold"> Price : $ {product.price} </p>
+
+            {saved ? (
+              <BookmarkSlashIcon
+                width={20}
+                height={20}
+                className="text-blue-600 cursor-pointer"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  UnSavedProductHandler(product._id);
+                }}
+              />
+            ) : (
               <BookmarkIcon
                 width={20}
                 height={20}
                 className="text-blue-600 cursor-pointer"
+                onClick={(event) => {
+                  event.stopPropagation();
+                  savedProductHandler(product._id);
+                }}
               />
-            </div>
+            )}
+          </div>
+          <Link to={`/products/${product._id}`}>
             <p className="italic">{product.description.slice(0, 80)} </p>
-          </Card>
-        </Link>
+          </Link>
+        </Card>
       </div>
     </>
   );
@@ -46,6 +91,8 @@ const Cards = ({ product }) => {
 
 Cards.propTypes = {
   product: PropTypes.any,
+  saved: PropTypes.bool,
+  getSaveProductLists: PropTypes.any,
 };
 
 export default Cards;
