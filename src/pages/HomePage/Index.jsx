929ad78bd78063d03312f120a37a473390b8052d
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-28 01:16:41 +0630

    refactor & props [func]

--- src/pages/HomePage/Index.jsx
+++ src/pages/HomePage/Index.jsx
@@ -2,7 +2,7 @@ import Hero from "../../components/HomePage/Hero";
 import Filter from "../../components/HomePage/Filter";
 import Cards from "../../components/HomePage/Cards";
 import { useCallback, useEffect, useState } from "react";
-import { getPublicProducts } from "../../api/product";
+import { getPublicProducts, getSavedProducts } from "../../api/product";
 import { message } from "antd";
 
 import { useSelector, useDispatch } from "react-redux";
@@ -11,28 +11,47 @@ import { RotatingLines } from "react-loader-spinner";
 
 const Index = () => {
   const [products, setProducts] = useState([]);
-
+  const [savedProducts, setSavedProducts] = useState([]);
   const dispatch = useDispatch();
   const { isProcessing } = useSelector((state) => state.reducer.isProcessing);
 
-  const getAllPublicProducts = useCallback(async () => {
-    dispatch(setProcessing(true));
-    try {
-      const response = await getPublicProducts();
-      if (response.isSuccess) {
-        setProducts(response.products);
-      } else {
-        throw new Error(response.message);
+  const fetchProducts = useCallback(
+    async (fetchFunction, setData) => {
+      dispatch(setProcessing(true));
+      try {
+        const response = await fetchFunction();
+        if (response.isSuccess) {
+          if (setData == setProducts) {
+            setData(response.products);
+          } else {
+            setData(response.savedProducts);
+          }
+        } else {
+          throw new Error(response.message);
+        }
+      } catch (err) {
+        message.error(err.message);
+      } finally {
+        dispatch(setProcessing(false));
       }
-    } catch (err) {
-      message.error(err.message);
-    }
-    dispatch(setProcessing(false));
-  }, [dispatch]);
+    },
+    [dispatch]
+  );
+
+  const getAllPublicProducts = useCallback(
+    () => fetchProducts(getPublicProducts, setProducts),
+    [fetchProducts]
+  );
+
+  const getSaveProducts = useCallback(
+    () => fetchProducts(getSavedProducts, setSavedProducts),
+    [fetchProducts]
+  );
 
   useEffect(() => {
     getAllPublicProducts();
-  }, [getAllPublicProducts]);
+    getSaveProducts();
+  }, [getAllPublicProducts, getSaveProducts]);
 
   return (
     <>
@@ -57,7 +76,12 @@ const Index = () => {
           <Filter products={products} setProducts={setProducts} />
           <div className="flex max-w-4xl flex-wrap mx-auto flex-row">
             {products.map((product) => (
-              <Cards key={product._id} product={product} />
+              <Cards
+                key={product._id}
+                product={product}
+                savedProducts={savedProducts}
+                getAllPublicProducts={getAllPublicProducts}
+              />
             ))}
           </div>
         </>
