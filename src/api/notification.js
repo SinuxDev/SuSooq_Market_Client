dd57685536678deb9ee96d3e68d17b37b6ddf236
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-29 02:13:59 +0630

    set: API Call For Notifications

---
+++ src/api/notification.js
@@ -0,0 +1,23 @@
+import { axiosInstance } from "./axiosInstance";
+
+// Combined API calls for notifications
+const fetchNotifications = async (endPoint, payload = {}, method = "get") => {
+  try {
+    const response = await axiosInstance({
+      method,
+      url: endPoint,
+      data: payload,
+      validateStatus: () => true,
+    });
+    return response.data;
+  } catch (err) {
+    return err.message;
+  }
+};
+
+// Add new notification
+export const pushNotification = (payload) =>
+  fetchNotifications("/push-notification", payload, "post");
+
+// Get all notifications
+export const getNotifications = () => fetchNotifications("/get-notifications");
