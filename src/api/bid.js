2dc5ac0bfe479ea8bd33be26ca037b9f285878ab
Aung Ye Htet <aung.atg1738@gmail.com>
2024-06-28 23:50:58 +0630

    set: API call for getBids

--- src/api/bid.js
+++ src/api/bid.js
@@ -1,9 +1,12 @@
 import { axiosInstance } from "./axiosInstance";
 
-// add new bids
-export const saveNewBid = async (payload) => {
+// Combined API calls for bids
+const fetchBids = async (endPoint, payload = {}, method = "get") => {
   try {
-    const response = await axiosInstance.post("/add-bid", payload, {
+    const response = await axiosInstance({
+      method,
+      url: endPoint,
+      data: payload,
       validateStatus: () => true,
     });
     return response.data;
@@ -11,3 +14,9 @@ export const saveNewBid = async (payload) => {
     return err.message;
   }
 };
+
+// get all bids by product ID
+export const getAllBids = (product_id) => fetchBids(`/get-bids/${product_id}`);
+
+// add new bids
+export const saveNewBid = (payload) => fetchBids("/add-bid", payload, "post");
